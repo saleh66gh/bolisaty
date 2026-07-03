339f507120ac9325862133be9155382859824634
saleh <mswq279@gmail.com>
2026-07-03 23:40:17 +0300

    Log Salla shipment response

--- App/salla/shipment.py
+++ App/salla/shipment.py
@@ -75,9 +75,13 @@ def handle_shipment_creating(db, payload: dict):
     response = {
         "success": True,
         "data": {
+            "id": int(tracking_number),
+            "shipping_number": tracking_number,
             "tracking_number": tracking_number,
+            "trackable": True,
             "tracking_link": tracking_number,
             "label": f"https://api.bolisaty.me/download-label/{label_data.order_number}",
+            "status": "created",
         }
     }
 
